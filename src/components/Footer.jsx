97df645390cb9e3332a0fc4ed6c4c8d139319670
Emanuele Magalhães <130506951+EmanueleMagalhaes@users.noreply.github.com>
2025-07-20 19:25:33 -0300

    atualização de dados do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,9 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container-footer">
-        <p className="footer-text">© 2023 Energia Solar 365. Todos os direitos reservados.</p>
-        
+        <p className="footer-text">© 2023 Energia Solar 365. Todos os direitos reservados. - <a href="mailto:energiasolar365@energia.com">energiasolar365@energia.com</a></p>
+        <p className="footer-text">CNPJ 12.345.678/0001-90 | Rua Bonita, 123 - Joinville - SC</p>
+
         <div className="social-icons">
           
         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
@@ -17,8 +18,8 @@ function Footer() {
     <FaWhatsapp size={24} color="#25D366" /> 
   </a>
         </div>
-        <p className="footer-text">Desenvolvido por Emanuele</p>
-      </div>
+        </div>
+
 
     </footer>
   );
